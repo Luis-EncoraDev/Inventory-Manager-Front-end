113361d746c9d3b953660d6fe6bf1ca72ec56925
Luis-EncoraDev <luis.arguelles@encora.com>
2025-05-18 19:26:07 -0500

    Started search component, Minor changes on DataTable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import DataTable from "./components/DataTable/DataTable.tsx"
+import SearchComponent from "./components/SearchComponent/SearchComponent.tsx"
 import './App.css'
 
 function App() {
 
   return (
     <div className="app">
+      <SearchComponent/>
       <DataTable/>
     </div>
   )
